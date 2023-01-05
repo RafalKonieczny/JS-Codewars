d5e8a94f3dacae2837810af9b697c9369300e83d
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-05 19:30:41 +0100

    Exercise 2

--- 8 kyu - exercises/script.js
+++ 8 kyu - exercises/script.js
@@ -11,7 +11,22 @@ function invert(array) {
 }
 
 invert([1,2,3,4,5]);
-invert([1,-2,3,-4,5]);
-invert([]);
-invert([0]);
+// invert([1,-2,3,-4,5]);
+// invert([]);
+// invert([0]);
+
+
+// 2. Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+repeatStr(3, "*");
+// repeatStr(5, "#");
+// repeatStr(2, "ha ");
 
