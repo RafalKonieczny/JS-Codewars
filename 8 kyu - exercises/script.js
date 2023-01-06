9a057c800581049600d77e21d71b98642129ff0f
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-06 11:30:38 +0100

    Exercise 7

--- 8 kyu - exercises/script.js
+++ 8 kyu - exercises/script.js
@@ -110,3 +110,23 @@ function howMuchILoveYou(nbPetals) {
 howMuchILoveYou(7);
 // howMuchILoveYou(3);
 // howMuchILoveYou(6);
+
+
+// 7. Abbreviate a Two Word Name
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+function abbrevName(name){
+  let words = name.split(" ");
+  let initials = words.map(word => word[0].toUpperCase());
+  return initials.join(".");
+}
+
+abbrevName("Sam Harris");
+// abbrevName("Patrick Feenan");
+// abbrevName("Evan Cole");
+// abbrevName("P Favuzzi");
+// abbrevName("David Mendieta");
