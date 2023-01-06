52f9e01148b6437eed8a33f479b3f6d59bd080cb
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-06 11:13:24 +0100

    Exercise 6

--- 8 kyu - exercises/script.js
+++ 8 kyu - exercises/script.js
@@ -78,3 +78,35 @@ function sumMix(x){
 sumMix([9, 3, '7', '3']);
 // sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]); 
 // sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']); 
+
+
+// 6. I love you, a little , a lot, passionately ... not at all
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// "I love you"
+// "a little"
+// "a lot"
+// "passionately"
+// "madly"
+// "not at all"
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+function howMuchILoveYou(nbPetals) {
+  const phrases = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all"
+  ];
+  let index = (nbPetals - 1) % phrases.length;
+
+  return phrases[index];
+}
+
+howMuchILoveYou(7);
+// howMuchILoveYou(3);
+// howMuchILoveYou(6);
