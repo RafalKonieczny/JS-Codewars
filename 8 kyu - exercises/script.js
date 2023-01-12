d94944e45c7f091fef1cc4a6bc50cec30ffedcef
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-12 20:33:50 +0100

    Exercises 11 and 12 in 8kyu

--- 8 kyu - exercises/script.js
+++ 8 kyu - exercises/script.js
@@ -8,7 +8,7 @@
 
 function invert(array) {
   return array.map(element => element * -1);
-}
+};
 
 invert([1,2,3,4,5]);
 // invert([1,-2,3,-4,5]);
@@ -24,7 +24,7 @@ invert([1,2,3,4,5]);
 
 function repeatStr (n, s) {
   return s.repeat(n);
-}
+};
 
 repeatStr(3, "*");
 // repeatStr(5, "#");
@@ -41,7 +41,7 @@ repeatStr(3, "*");
 
 function addLength(str) {
   return str.split(" ").map((element) => element + " " + element.length);
-}
+};
 
 addLength('apple ban');
 // addLength('you will win');
@@ -57,7 +57,7 @@ function otherAngle(a, b) {
   let sum = a + b;
   let thirdAngle = 180 - sum;
   return thirdAngle;
-}
+};
 
 otherAngle(30, 60);
 // otherAngle(60, 60);
@@ -66,6 +66,7 @@ otherAngle(30, 60);
 
 
 // 5. Sum Mixed Array
+
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 // Return your answer as a number.
 
@@ -73,7 +74,7 @@ function sumMix(x){
   return x.reduce((accumulator, currentValue) => {
     return accumulator + Number(currentValue);
   }, 0); 
-}
+};
 
 sumMix([9, 3, '7', '3']);
 // sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]); 
@@ -81,6 +82,7 @@ sumMix([9, 3, '7', '3']);
 
 
 // 6. I love you, a little , a lot, passionately ... not at all
+
 // Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
 
 // "I love you"
@@ -105,7 +107,7 @@ function howMuchILoveYou(nbPetals) {
   let index = (nbPetals - 1) % phrases.length;
 
   return phrases[index];
-}
+};
 
 howMuchILoveYou(7);
 // howMuchILoveYou(3);
@@ -113,6 +115,7 @@ howMuchILoveYou(7);
 
 
 // 7. Abbreviate a Two Word Name
+
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 // The output should be two capital letters with a dot separating them.
 // It should look like this:
@@ -123,7 +126,7 @@ function abbrevName(name){
   let words = name.split(" ");
   let initials = words.map(word => word[0].toUpperCase());
   return initials.join(".");
-}
+};
 
 abbrevName("Sam Harris");
 // abbrevName("Patrick Feenan");
@@ -133,15 +136,10 @@ abbrevName("Sam Harris");
 
 
 // 8. Count by X
+
 // Create a function with two arguments that will return an array of the first n multiples of x.
-
 // Assume both the given number and the number of times to count will be positive numbers greater than 0.
-
 // Return the results as an array or list ( depending on language ).
-
-// Examples
-// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10];
-// countBy(2,5) === [2,4,6,8,10];
 
 function countBy(x, n) {
   let z = [];
@@ -156,6 +154,7 @@ countBy(1,10);
 
 
 // 9. Grasshopper - Debug sayHello
+
 // Debugging sayHello function
 // The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
 
@@ -170,6 +169,7 @@ sayHello('Mr. Spock');
 
 
 // 10. Transportation on vacation
+
 // After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
 // You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
@@ -183,10 +183,42 @@ function rentalCarCost(d) {
   } else {
     return d * 40;
   }
-}
+};
 
 rentalCarCost(1);
 // rentalCarCost(2);
 // rentalCarCost(3);
 // rentalCarCost(4);
 // rentalCarCost(5);
+
+
+// 11. You only need one - Beginner
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  return a.includes(x);
+};
+
+check([66, 101], 66);
+// check([101, 45, 75, 105, 99, 107], 107);
+
+
+// 12. Powers of 2
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+};
+
+powersOfTwo(1);
+// powersOfTwo(2);
+// powersOfTwo(4);
+
