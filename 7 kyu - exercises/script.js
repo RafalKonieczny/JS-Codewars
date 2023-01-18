b93c2c38e38a9286a02e7b555b073c26c53bcdcb
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-18 23:08:02 +0100

    Exercise 7 in 7 kyu

--- 7 kyu - exercises/script.js
+++ 7 kyu - exercises/script.js
@@ -131,4 +131,20 @@ reverseWords('The quick brown fox jumps over the lazy dog.');
 // reverseWords('apple')
 
 
+// 7. Make a function that does arithmetic!
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+// The four operators are "add", "subtract", "divide", "multiply".
 
+function arithmetic(a, b, operator) {
+  const ops = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+  };
+  return ops[operator](a, b);
+}
+
+arithmetic(1, 2, "add");
+// arithmetic(8, 2, "subtract");
