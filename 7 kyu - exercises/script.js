245104a60961247954426fda79f6dbdb2fd8780e
Rafał Konieczny <rkonieczny87@gmail.com>
2023-01-30 23:00:28 +0100

    Exercise 10 in 7 kyu

--- 7 kyu - exercises/script.js
+++ 7 kyu - exercises/script.js
@@ -26,20 +26,11 @@ longest("aretheyhere", "yestheyarehere");
 
 
 // 2. Remove the minimum
-// The museum of incredible dull things
 // The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
-
 // However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
-
 // Task
 // Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
-
 // Don't change the order of the elements that are left.
-
-// Examples
-// * Input: [1,2,3,4,5], output = [2,3,4,5]
-// * Input: [5,3,2,1,4], output = [5,3,2,4]
-// * Input: [2,2,1,2,1], output = [2,2,2,1]
 
 //(first option)
 
@@ -175,3 +166,15 @@ function roundToNext5(n){
 // }
 
 roundToNext5(29);
+
+
+// 10. Is this a triangle?
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c) {
+  return a + b > c && a + c > b && b + c > a
+}
+
+isTriangle(1,2,2);
+// isTriangle(7,2,2);
