047b89746263a97fc4b18be2f1e276528c62e65a
Rafał Konieczny <rkonieczny87@gmail.com>
2023-03-31 23:16:09 +0200

    Exercise 11 in 7 kyu

--- 7 kyu - exercises/script.js
+++ 7 kyu - exercises/script.js
@@ -178,3 +178,20 @@ function isTriangle(a,b,c) {
 
 isTriangle(1,2,2);
 // isTriangle(7,2,2);
+
+
+// 11. Ones and Zeros
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+// Examples: 
+// Testing: [0, 0, 0, 1] ==> 1
+// However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2);
+};
+
+// binaryArrayToNumber([0,0,0,1]);
+binaryArrayToNumber([0,1,0,1]);
+
+
